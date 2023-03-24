1728b475c5adc4944634264ea549aa203b5a52b0
cooolr <32413353+cooolr@users.noreply.github.com>
2023-03-24 12:10:08 +0800

    Create telegram_api.py

---
+++ telegram_api.py
@@ -0,0 +1,69 @@
+import os
+import requests
+from typing import List, Tuple
+from uuid import uuid1
+
+from config import TELEGRAM_TOKEN, SPEECH_TO_TEXT
+from notion_api import insert
+from speech import speech
+
+
+def get_file_path(file_id: str) -> str:
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/getFile?file_id={file_id}"
+    r = requests.get(url)
+    return r.json()["result"]["file_path"]
+
+def get_file_url(file_path: str) -> str:
+    url = f"https://api.telegram.org/file/bot{TELEGRAM_TOKEN}/{file_path}"
+    return url
+
+def speech_to_text(file_url):
+    r = requests.get(file_url)
+    filename1 = str(uuid1()) + ".oga"
+    filename2 = str(uuid1()) + ".wav"
+    with open(filename1, "wb") as f:
+        f.write(r.content)
+    os.system(f"ffmpeg -i {filename1} -ar 16000 {filename2}")
+    text = speech(filename2)
+    os.remove(filename1)
+    os.remove(filename2)
+    return text
+
+def process_telegram_message(message: dict) -> None:
+    """Process a Telegram message and insert it into the Notion database."""
+    text = message.get("text", "") or message.get("caption", "")
+    link_list = []
+
+    if message.get("entities"):
+        for item in message["entities"]:
+            if item.get("url"):
+                link_list.append((item['offset'], item['length'], text[item['offset']:item['offset'] + item['length']], item['url']))
+
+    if message.get("document"):
+        file_type = message["document"]["mime_type"]
+        file_id = message["document"]["file_id"]
+        file_url = get_file_url(get_file_path(file_id))
+        insert(text, file_type, file_url, link_list)
+    elif message.get("photo"):
+        file_type = "image/jpeg"
+        file_id = message["photo"][-1]["file_id"]
+        file_url = get_file_url(get_file_path(file_id))
+        insert(text, file_type, file_url, link_list)
+    elif message.get("video_note"):
+        file_type = "video/mp4"
+        file_id = message["video_note"]["file_id"]
+        file_url = get_file_url(get_file_path(file_id))
+        insert(text, file_type, file_url, link_list)
+    elif message.get("voice"):
+        file_type = "audio/ogg"
+        file_id = message["voice"]["file_id"]
+        file_url = get_file_url(get_file_path(file_id))
+        if SPEECH_TO_TEXT:
+            text = speech_to_text(file_url)
+        insert(text, file_type, file_url, link_list)
+    else:
+        text = text + "\n"
+        insert(text, link_list=link_list)
+
+if __name__ == "__main__":
+    app.run()
